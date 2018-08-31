5e5b993bb32442ca189ab11dec2ebf93af813af8
sliew <haha.liews@gmail.com>
2018-08-31 01:09:45 -0400

    added server files and server dependencies

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db  = mongoose.connect('mongodb://localhost:27017/myapp');
+
+app.put('/api/sendmessage', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
