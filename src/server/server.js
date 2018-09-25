58913b21673e11e00c353347c1ccde86b9503e17
sliew <haha.liews@gmail.com>
2018-09-25 13:14:14 -0400

    updated design

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const Message = require('./schemas/MessageSchema');
 
 const app = express();
 const port = 5000;
